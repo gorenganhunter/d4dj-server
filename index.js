49ae714620906a2627e2031b6bd55b97b3a89315
gorenganhunter <gorengandev@gmail.com>
2024-07-06 19:33:39 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const { Sonolus, SonolusSpaShare } = require("@sonolus/express")
+const express = require("express")
+
+const sonolus = new Sonolus()
+sonolus.load('./pack')
+
+const sonolusShare = new SonolusSpaShare('./public')
+
+const port = 3000
+const app = express()
+
+app.use(sonolus.router)
+app.use(sonolusShare.router)
+
+app.listen(port, () => {
+    console.log('Server listening at port', port)
+})
