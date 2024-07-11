88878dbc26d9dc434c0dd87f1d4c2054d6b33f76
gorenganhunter <gorengandev@gmail.com>
2024-07-11 09:21:57 +0700

    fix missplaced effect

--- index.js
+++ index.js
@@ -6,8 +6,48 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("@sonolus/express");
 const core_1 = require("@sonolus/core");
 const express_2 = __importDefault(require("express"));
-const sonolus = new express_1.Sonolus({ fallbackLocale: "en", address: "https://d4dj.sonolus.gorenganhunter.my.id" });
-sonolus.load('./pack');
+// import db from "./pack/db.json";
+// const artists: any[] = [];
+// const category: any[] = [];
+// const diff = [Text.Easy, Text.Normal, Text.Hard, Text.Expert];
+// db.levels.forEach((level) => {
+//     if (!artists.find(({ jp }) => jp === level.artists.jp))
+//         artists.push(level.artists);
+//     if (!category.find(({ jp }) => jp === level.tags[1].title.jp))
+//         category.push(level.tags[1].title);
+// });
+const sonolus = new express_1.Sonolus({
+    address: "https://d4dj.sonolus.gorenganhunter.my.id",
+    // level: {
+    //     searches: {
+    //         advanced: {
+    //             title: { en: Text.Advanced },
+    //             options: {
+    //                 artists: {
+    //                     type: "multi",
+    //                     name: { en: Text.Artists },
+    //                     values: [...artists.map((title) => ({ title, def: true }))],
+    //                 },
+    //                 category: {
+    //                     type: "multi",
+    //                     name: { en: Text.Category },
+    //                     values: [...category.map((title) => ({ title, def: true }))],
+    //                 },
+    //                 diff: {
+    //                     type: "multi",
+    //                     name: { en: Text.Difficulty },
+    //                     values: [...diff.map((jp) => ({ title: { jp }, def: true }))],
+    //                 },
+    //                 minLevel: {
+    //                     type: "slider",
+    //                     name: { en: Text }
+    //                 }
+    //             },
+    //         },
+    //     }
+    // }
+});
+sonolus.load("./pack");
 sonolus.serverInfoHandler = ({ session }) => ({
     title: sonolus.title,
     description: sonolus.description,
@@ -21,7 +61,7 @@ sonolus.serverInfoHandler = ({ session }) => ({
         { type: "effect" },
         { type: "particle" },
         { type: "engine" },
-    ]
+    ],
 });
 function shuffle(array) {
     let currentIndex = array.length;
@@ -32,7 +72,8 @@ function shuffle(array) {
         currentIndex--;
         // And swap it with the current element.
         [array[currentIndex], array[randomIndex]] = [
-            array[randomIndex], array[currentIndex]
+            array[randomIndex],
+            array[currentIndex],
         ];
     }
 }
@@ -40,39 +81,60 @@ sonolus.level.detailsHandler = ({ itemName }) => {
     const item = sonolus.level.items.find(({ name }) => name === itemName);
     if (!item)
         return undefined;
-    const otherDiff = sonolus.level.items.filter(({ name }) => (name.split("-")[1] == itemName.split("-")[1]) && (name !== itemName));
-    const sameArtists = sonolus.level.items.filter(({ artists, name }) => (artists.jp == item.artists.jp) && (name !== itemName));
-    const sameAuthor = sonolus.level.items.filter(({ author, name }) => (author.jp == item.author.jp) && (name !== itemName));
-    const sameRating = sonolus.level.items.filter(({ rating, name }) => (rating == item.rating) && (name !== itemName));
-    const sameCategory = sonolus.level.items.filter(({ tags, name }) => (tags[1].title.jp == item.tags[1].title.jp) && (name !== itemName));
+    const otherDiff = sonolus.level.items.filter(({ name }) => name.split("-")[1] == itemName.split("-")[1] && name !== itemName);
+    const sameArtists = sonolus.level.items.filter(({ artists, name }) => artists.jp == item.artists.jp && name !== itemName);
+    const sameAuthor = sonolus.level.items.filter(({ author, name }) => author.jp == item.author.jp && name !== itemName);
+    const sameRating = sonolus.level.items.filter(({ rating, name }) => rating == item.rating && name !== itemName);
+    const sameCategory = sonolus.level.items.filter(({ tags, name }) => tags[1].title.jp == item.tags[1].title.jp && name !== itemName);
     shuffle(sameAuthor);
     shuffle(sameRating);
     shuffle(sameArtists);
     shuffle(sameCategory);
-    return { item, description: item.description, hasCommunity: false, sections: [
+    return {
+        item,
+        description: item.description,
+        hasCommunity: false,
+        sections: [
             {
                 title: { en: core_1.Text.OtherDifficulties },
-                items: otherDiff
+                items: otherDiff,
             },
             {
                 title: { en: core_1.Text.SameArtists },
-                items: sameArtists.slice(0, 5)
+                items: sameArtists.slice(0, 5),
             },
             {
                 title: { en: core_1.Text.SameCategory },
-                items: sameCategory.slice(0, 5)
+                items: sameCategory.slice(0, 5),
             },
             {
                 title: { en: core_1.Text.SameRating },
-                items: sameRating.slice(0, 5)
+                items: sameRating.slice(0, 5),
             },
             {
                 title: { en: core_1.Text.SameAuthor },
-                items: sameAuthor.slice(0, 5)
+                items: sameAuthor.slice(0, 5),
             },
-        ], leaderboards: [] };
+        ],
+        leaderboards: [],
+    };
 };
-const sonolusShare = new express_1.SonolusSpaShare('./public');
+sonolus.level.infoHandler = (ctx) => {
+    const expert = sonolus.level.items.filter(({ name }) => parseInt(name.split("-")[2]) === 4);
+    shuffle(expert);
+    return {
+        sections: [
+            {
+                title: { en: core_1.Text.Random },
+                icon: core_1.Icon.Shuffle,
+                items: expert.slice(0, 10),
+            },
+        ],
+        searches: [],
+        banner: sonolus.banner,
+    };
+};
+const sonolusShare = new express_1.SonolusSpaShare("./public");
 const sonolusRedirect = new express_1.SonolusRedirectShare("d4dj.sonolus.gorenganhunter.my.id");
 const port = 8080;
 const app = (0, express_2.default)();
@@ -80,5 +142,5 @@ app.use(sonolus.router);
 app.use(sonolusShare.router);
 app.use(sonolusRedirect.router);
 app.listen(port, () => {
-    console.log('Server listening at port', port);
+    console.log("Server listening at port", port);
 });
