3d04315de3cf7c038d8bef3a03b1094dc2a053a5
gorenganhunter <gorengandev@gmail.com>
2024-10-31 18:13:31 +0700

    bersihin yang ga kepake

--- api/object.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.notUndefined = exports.mapValues = exports.fromEntries = exports.entries = void 0;
-exports.entries = Object.entries;
-exports.fromEntries = Object.fromEntries;
-const mapValues = (object, callback) => (0, exports.fromEntries)((0, exports.entries)(object).map(([k, v], i) => [k, callback(k, v, i)]));
-exports.mapValues = mapValues;
-const notUndefined = (obj) => obj !== undefined;
-exports.notUndefined = notUndefined;
